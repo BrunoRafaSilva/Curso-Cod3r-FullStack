3b0d91c916ebc3c2266399ac7ac13fcebf230122
Bruno <bruno.rafael.lucena@gmail.com>
2022-05-29 22:31:06 -0300

    exercicios
    atualizações de arquivos

---
+++ CURSO DA UDEMY/4-Função Exercicios/exercicio3.js
@@ -0,0 +1,12 @@
+//Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+//function elevacaoPelvica(n1,n2){
+//    console.log(`A elevação pélvica fica assim: ${n1**n2}`);
+//}
+//elevacaoPelvica(3,2)
+
+var thing;
+let func = (str = 'noarg')=>{
+    console.log(str);
+}
+func(thing);
+func(null);
---
+++ CURSO DA UDEMY/4-Função Exercicios/exercicio4.js
@@ -0,0 +1,6 @@
+function resurest(n1,n2){
+	console.log(`O resultado da divisão será ${n1/n2}`);
+	console.log(`O resto da divisão do primeiro número informado será: ${n2%2}`)
+}
+
+resurest(8,2);
