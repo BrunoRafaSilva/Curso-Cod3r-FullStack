6adda53be62fa6dbf40df7f07f10e89b2ae01a06
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-03-06 19:31:59 -0300

    udemy javascript

---
+++ Curso Cod3r/5-Objeto/DefinicoesOO.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!!
+
+//Procedural - funções que manipulam dados
+processamento(valor1, valor2, valor3)
+
+//Orientação a Objetos - Dados e dentro desses dados há funções
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    valor3,
+    processamento(){
+        //.....
+    }
+}
+
+objeto.processamento() //Foco passou a ser o objeto
+/* 
+1.Abstração
+2.Emcapsulamento
+3.Herança
+4.Polimorfismo
+*/
--- Curso Cod3r/5-Objeto/oo.js
+++
@@ -1,3 +0,0 @@
-// CÓDIGO NÃO EXECUTÁVEL!!!!
-
-//Procedural
