da967a8a4913fdc351b54abfa8d631dfd59e630d
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-05-26 14:31:32 -0300

    Update 7-maps2.js

--- Curso Cod3r/6-Array/7-maps2.js
+++ Curso Cod3r/6-Array/7-maps2.js
@@ -1,9 +1,14 @@
 const carrinho = [
     '{"nome": "Borracha", "preco": 3.45 }',
-    '{"nome: "Caderno", "preco": 13.90}',
+    '{"nome": "Caderno", "preco": 13.90}',
     '{"nome": "Kit de Lapis", "preco": 13.90}',
     '{"nome": "Caneta", "preco": 7.50}'
 ]
 
 //Retornar um array apenas com os preços
 
+const paraObjeto = json => JSON.parse(json) //converte um texto para objeto
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
