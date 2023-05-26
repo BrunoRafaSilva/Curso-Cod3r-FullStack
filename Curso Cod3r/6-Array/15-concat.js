70e000cdafa8f077487e9c4ecd26fc5137f51e74
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-05-26 18:42:09 -0300

    update

--- Curso Cod3r/6-Array/15-concat.js
+++ Curso Cod3r/6-Array/15-concat.js
@@ -1,4 +1,6 @@
 const filhas = ['Alexia', 'Chirlene']
 const filhos = ['Tony', 'Slambre']
 const todos = filhas.concat(filhos)
-console.log(todos)
+console.log(todos,'todes')
+
+console.log(['a','b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
