30a380a5f0daf3de17096b2fb070ca9b126f7109
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-05-26 14:27:11 -0300

    Update 7-maps2.js

--- Curso Cod3r/6-Array/7-maps2.js
+++ Curso Cod3r/6-Array/7-maps2.js
@@ -1,8 +1,9 @@
 const carrinho = [
     '{"nome": "Borracha", "preco": 3.45 }',
     '{"nome: "Caderno", "preco": 13.90}',
-    '{"nome": "Kit de Lapis", "preco: 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 13.90}',
     '{"nome": "Caneta", "preco": 7.50}'
 ]
 
 //Retornar um array apenas com os preços
+
