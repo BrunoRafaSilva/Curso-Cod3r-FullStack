0651e0888a28d6d98edaafc8e8158b472ade8ab9
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-05-26 14:26:46 -0300

    up

---
+++ Curso Cod3r/6-Array/7-maps2.js
@@ -0,0 +1,8 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45 }',
+    '{"nome: "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco: 13.90}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+//Retornar um array apenas com os preços
