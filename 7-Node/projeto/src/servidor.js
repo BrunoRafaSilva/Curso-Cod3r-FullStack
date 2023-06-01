98e9e6c11c3526ebf0bcb705115bed757a6a9517
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-06-01 18:20:29 -0300

    postman

--- 7-Node/projeto/src/servidor.js
+++ 7-Node/projeto/src/servidor.js
@@ -2,31 +2,42 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
-const bancoDeDados = require('./bancoDeDados.js')
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 
-/*app.get('/produtos', (req, res, next) => {
-    console.log('Middleware 1...')
-    next()
-})*/
+app.use(bodyParser.urlencoded({extended :true}))
 
 
-app.get('/produtos', (req, res, next) => {
-    //res.send({ nome: 'Notebook', preco: 123.45}) //converte para JSON
-    res.send(bancoDeDados.getProdutos)
-})//se colocar .use, ele vai responder qualquer metodo(tem que demover o /produtos)
+app.get('/produtos' , (req , res , next) => {
+    res.send(bancoDeDados.getProdutos()) //conversao automatica para json
+})
 
-app.get('/produtos/:id', (req, res, next) => {
+app.get('/produtos/:id' , (req ,res , next) => {
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('/produtos', (req, res, next) => {
-    const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+app.post('/produtos' , (req , res , next) => {
+    const produto = bancoDeDados.salvarProduto ({
+        nome: req.body.nome,
         preco: req.body.preco
     })
-    res.send(produto) // JSON
+    res.send(produto) //json
+})
+
+app.put('/produtos/:id' , (req , res , next) => {
+    const produto = bancoDeDados.salvarProduto ({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //json
+})
+
+app.delete('/produtos/:id' , (req , res , next) => {
+    const produto = bancoDeDados.excluirProduto (req.params.id)
+    res.send(produto) //json
 })
 
 app.listen(porta, () => {
-    console.log(`Servidor está executando na porta ${porta}.`)
+    console.log(`Servidor executando na porta ${porta}`)
 })
