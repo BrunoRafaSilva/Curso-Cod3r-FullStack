8bc184ac3e71135f5870946547c033c2954cc0e9
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-08-07 20:03:42 -0300

    3

--- 8-Esnext/16-promise_3.js
+++ 8-Esnext/16-promise_3.js
@@ -5,7 +5,7 @@ function gerarNumerosEntre(min, max) {
 
     return new Promise(resolve => {
         const fator = max - min + 1
-        const aleatorio = parseInt(Math.random() * fator)
+        const aleatorio = parseInt(Math.random() * fator) + min
         resolve(aleatorio)
     })
 }
